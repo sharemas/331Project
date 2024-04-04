a27868ef13533d7ffc3f8cea3d502e558c3ab096
Noah Kurtz <85361179+TheLandStander@users.noreply.github.com>
2024-04-04 13:19:03 -0400

    added methods

--- src/Student.java
+++ src/Student.java
@@ -1,50 +1,75 @@
+
+
 import java.util.ArrayList;
+
 
 public class Student {
    public String name;
    private int SSN;
-   private  int studentID;
-   private static int studentCount = 0;
+   private int studentID;
    private String address;
    private String email;
    public double GPA;
    public String contactName;
    private String contactPhoneNumber;
    private String contactAddress;
-   public ArrayList<Enrollment> StudentEnrollments;
+   public ArrayList<Enrollment> StudentEnrollments = new ArrayList<>();
    
-  public Student(String name, int SSN,String address,String email, double GPA,
+  public Student(String name, int SSN,int studentID,String address,String email, double GPA,
          String contactName, String contactPhoneNumber, String contactAddress){
          this.name = name;
          this.SSN = SSN;
-         this.studentID = studentCount;
-         studentCount++;
+         this.studentID = studentID;
          this.address = address;
          this.email = email;
          this.GPA = GPA;
          this.contactName = contactName;
          this.contactPhoneNumber = contactPhoneNumber;
          this.contactAddress = contactAddress;
-         this.StudentEnrollments = new ArrayList<>();
+         
   }
-   
+        public void enrollStudent(Semester semester, Course course) {
+          course.AddStudent(this, semester);
+	}
+	
+	public void unenrollStudent(Enrollment enrollment) {
+            for(int i = 0; i < StudentEnrollments.size(); i++){
+            if(StudentEnrollments.get(i) == enrollment){
+            enrollment.course.EnrolledStudents.remove(this);
+            }
+            }
+            StudentEnrollments.remove(enrollment);
+	}
+        
+        public String genStudentCourses(String CourseName){
+        String gen = "";
+        for(int i = 0; i < StudentEnrollments.size(); i++){
+        if(StudentEnrollments.get(i).course.courseName.equals(CourseName)){
+        gen += (i +1) + ":\t" + StudentEnrollments.get(i).course.courseName;
+        }
+        }
+        return gen;
+        }
+        
+        
+  
     public int GetSSN(){
-    	return this.SSN;
+    return this.SSN;
     }
     
     public int GetStudentID(){
-    	return this.studentID;
+    return this.studentID;
     }
     
     public String GetEmail(){
-    	return this.email;
+    return this.email;
     }
     public void SetEmail(String email){
-    	this.email = email;
+    this.email = email;
     }
     
     public String GetAddress(){
-    	return this.address;
+    return this.address;
     }
     
     public void SetAddress(String address){
@@ -52,18 +77,18 @@ public class Student {
     }
     
     public String GetContactPhone(){
-    	return this.contactPhoneNumber;
+    return this.contactPhoneNumber;
     }
     
     public void SetContactPhone(String contactPhoneNumber){
-    	this.contactPhoneNumber = contactPhoneNumber;
+    this.contactPhoneNumber = contactPhoneNumber;
     }
     
     public String GetContactAddress(){
-    	return this.contactAddress;
+    return this.contactAddress;
     }
    
     public void SetContactAddress(String contactAddress){
-    	this.contactAddress = contactAddress;
+    this.contactAddress = contactAddress;
     }
 }
