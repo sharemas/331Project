9895dd265aad39d0a9ec2239da79a0f35047ec13
Noah Kurtz <85361179+TheLandStander@users.noreply.github.com>
2024-04-04 13:16:07 -0400

    Update Enrollment.java

--- src/Enrollment.java
+++ src/Enrollment.java
@@ -4,21 +4,13 @@ public class Enrollment{
 	public Semester semester;
 	public Course course;
 	
+	public class Enrollment{
+	public Semester semester;
+	public Course course;
 	public Enrollment(Semester semester, Course course) {
 		this.semester = semester;
 		this.course = course;
 	}
 	
-	public void enrollStudent(Student student) {
-		//finish method here
-		//Student.enrolledCourseList.add(new Enrollment(semester, course));
-	}
 	
-	public void unenrollStudent(Student student) {
-		// finish method here 
-		//List<Enrollment> enrollmentList= Student.enrollCourseList();
-		//for (Enrollment enrollment: enrollmentList ) {
-		//	if 
-	//	}
-	}
 }
