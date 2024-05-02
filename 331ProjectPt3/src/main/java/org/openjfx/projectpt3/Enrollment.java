3b7c5745db0cd76081928ce02d28501b07018a8d
sharemas <143344224+sharemas@users.noreply.github.com>
2024-05-02 16:02:27 -0400

    Update Enrollment.java

--- 331ProjectPt3/src/main/java/org/openjfx/projectpt3/Enrollment.java
+++ 331ProjectPt3/src/main/java/org/openjfx/projectpt3/Enrollment.java
@@ -5,7 +5,7 @@ public class Enrollment {
 	public Semester semester;
 	public Course course;
         public Student student;
-	public Enrollment(Semester semester, Course course,Student student) {
+	public Enrollment(Semester semester, Course course, Student student) {
 		this.semester = semester;
 		this.course = course;
                 this.student = student;
