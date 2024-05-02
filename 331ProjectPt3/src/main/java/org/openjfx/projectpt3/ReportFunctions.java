ad93d2d4078fc65a72f6706c8214de9482c4617e
sharemas <143344224+sharemas@users.noreply.github.com>
2024-05-02 16:03:34 -0400

    Create ReportFunctions.java

---
+++ 331ProjectPt3/src/main/java/org/openjfx/projectpt3/ReportFunctions.java
@@ -0,0 +1,56 @@
+package org.openjfx.projectpt3;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReportFunctions {
+    public static ArrayList<Department> departmentList = new ArrayList<>();
+    public static ArrayList<Course> courseList = new ArrayList<>();
+    public static ArrayList<Student> StudentList = new ArrayList<>();
+    public static ArrayList<Faculty> FacultyList = new ArrayList<>();
+    public static ArrayList<Semester> SemesterList = new ArrayList<>();
+    public static ArrayList<Schedule> ScheduleList = new ArrayList<>();
+    public static ArrayList<Enrollment> EnrollmentList = new ArrayList<>();
+
+
+   public static String FacultyCourses(Semester semester, Faculty faculty){
+        	String gen = "During " + semester + ", " + faculty + " is teaching: ";
+        	for (int i = 0; i < ScheduleList.size(); i++){
+        		
+        		if (faculty == ScheduleList.get(i).faculty && semester == ScheduleList.get(i).semester){
+        			gen += "\n" + String.format("%-10s%40s", ScheduleList.get(i).course.prefix + ScheduleList.get(i).course.number, ScheduleList.get(i).course.courseName);
+        		}
+        }
+        return gen;
+        }
+    
+    //Design course which is taught in this semester, takes an input for semester
+    //searches through schedule for courses and genrates string report based on the semester object that was found
+    public static String CourseSemester(Semester semester, Student student){
+        	String gen = "During " + semester + ", " + student + " is taking: ";     
+
+        		for(int i = 0; i < EnrollmentList.size(); i++){
+        			if(semester == EnrollmentList.get(i).semester && student ==  EnrollmentList.get(i).student){
+        				gen += "\n" + String.format("%-10s%40s", EnrollmentList.get(i).course.prefix + EnrollmentList.get(i).course.number, EnrollmentList.get(i).course.courseName);
+        			}	
+        		}
+        return gen;
+        }
+    
+    public static String StudentCourse(Course course, Semester semester){
+    String gen = "During " + semester + ", " + course + " contains: ";     
+        	int count = 0;
+
+        		for(int i = 0; i < EnrollmentList.size(); i++){
+        			if(semester == EnrollmentList.get(i).semester && course ==  EnrollmentList.get(i).course){
+        				count ++;
+        				gen += "\n" + String.format("%-10s%40s", EnrollmentList.get(i).student.GetEmail(),EnrollmentList.get(i).student.name);
+        			}	
+        		}
+        return gen;
+    }
+    
+   
+    
+}
+    
