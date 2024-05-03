05bc969b3e77a8cbfd1bf3549e174d19d56c4116
sharemas <143344224+sharemas@users.noreply.github.com>
2024-05-03 11:47:50 -0400

    Update Department.java

--- 331ProjectPt3/src/main/java/org/openjfx/projectpt3/Department.java
+++ 331ProjectPt3/src/main/java/org/openjfx/projectpt3/Department.java
@@ -1,14 +1,19 @@
-package org.openjfx.projectpt3;
+package com.noahkurtz.databasep4;
+
 import java.util.ArrayList;
 
 public class Department {
     public String departmentName;
     public ArrayList<Faculty> FacultyList;
+    public int departmentID;
+    private static int Count = 0;
 
     public Department(String departmentName) {
 
         this.departmentName = departmentName;
         this.FacultyList = new ArrayList<>();
+        this.departmentID = Count + 1;
+        Count++;
     }
     
     @Override
