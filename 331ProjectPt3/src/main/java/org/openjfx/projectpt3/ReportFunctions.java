609c9ff56ef1025c52037f59e6a1382124834005
sharemas <143344224+sharemas@users.noreply.github.com>
2024-05-03 11:48:55 -0400

    Update ReportFunctions.java

--- 331ProjectPt3/src/main/java/org/openjfx/projectpt3/ReportFunctions.java
+++ 331ProjectPt3/src/main/java/org/openjfx/projectpt3/ReportFunctions.java
@@ -1,4 +1,6 @@
-package org.openjfx.projectpt3;
+package com.noahkurtz.databasep4;
+
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -50,7 +52,260 @@ public class ReportFunctions {
         return gen;
     }
     
+    
+       public static void updateDepartments() throws SQLException {
+        String query = "SELECT * FROM DEPARTMENT";  
+   
+        try {
+            App.runDBQuery(query, 'r'); 
+        
+            departmentList.clear(); 
+        
+            while (App.jsqlResults.next()) {
+                String name = App.jsqlResults.getString(2);  
+                departmentList.add(new Department(name)); 
+            }
+        
+        } finally {
+      
+        if (App.jsqlStmt != null) {
+            App.jsqlStmt.close(); 
+        }
+        if (App.jsqlConn != null) {
+            App.jsqlConn.close(); 
+        }
+    }
+}
+      
+           public static void updateCourseList() throws SQLException {
+                String query = "SELECT * FROM COURSE";  
+                App.runDBQuery(query, 'r');  
+
+                courseList.clear();  
+
+                try {
+                    while (App.jsqlResults.next()) {
+                    String prefix = App.jsqlResults.getString(2);  
+                    int number =  App.jsqlResults.getInt(3); 
+                    String courseName =  App.jsqlResults.getString(4);
+                    int daysWeek =  App.jsqlResults.getInt(5); 
+                    String startTime =  App.jsqlResults.getString(6);  
+                    String endTime =  App.jsqlResults.getString(7);  
+                    int numCredits =  App.jsqlResults.getInt(8);
+
+            // Add the course object to the list
+            courseList.add(new Course(prefix, number, courseName, daysWeek, startTime, endTime, numCredits));
+        }
+    } finally {
+        
+        if (App.jsqlStmt != null) {
+            App.jsqlStmt.close();
+        }
+        if (App.jsqlConn != null) {
+            App.jsqlConn.close();
+        }
+    }
+}
+
+            public static void updateStudentList() throws SQLException {
+                String query = "SELECT * FROM STUDENT"; 
+                App.runDBQuery(query, 'r');  
+                StudentList.clear();  
+
+                try {
+                    while (App.jsqlResults.next()) {
+                    
+                    int ssn = App.jsqlResults.getInt(2);
+                    String studentName =  App.jsqlResults.getString(3);
+                    String address =  App.jsqlResults.getString(4); 
+                    String email =  App.jsqlResults.getString(5); 
+                    double gpa =  App.jsqlResults.getDouble(6); 
+                    String contactName =  App.jsqlResults.getString(7);
+                    String contactPhone =  App.jsqlResults.getString(8); 
+                    String contactAddress =  App.jsqlResults.getString(9); 
+                    
+            // Add the course object to the list
+            StudentList.add(new Student(studentName, ssn, address, email, gpa, contactName, contactPhone, contactAddress));
+        }
+    } finally {
+        
+        if (App.jsqlStmt != null) {
+            App.jsqlStmt.close();
+        }
+        if (App.jsqlConn != null) {
+            App.jsqlConn.close();
+        }
+    }
+}
+            
+             public static void updateFacultyList() throws SQLException {
+        String query = "SELECT * FROM FACULTY";
+
+        App.runDBQuery(query, 'r'); 
+
+        FacultyList.clear(); 
+
+        try {
+            while (App.jsqlResults.next()) {
+                // Extract data from the result set
+                String name = App.jsqlResults.getString(2);
+                String email = App.jsqlResults.getString(3);
+                String buildingName = App.jsqlResults.getString(4);
+                int officeNum = App.jsqlResults.getInt(5);
+                String phoneNum = App.jsqlResults.getString(6);
+                int departmentID = App.jsqlResults.getInt(7);
+                String position = App.jsqlResults.getString(8);
+                
+                Department department = null;
+                
+                for (Department currDep: departmentList){
+                    if (currDep.departmentID == departmentID){
+                        department = currDep; 
+                    } 
+                }
+
+                // Add the Faculty object to the list
+                Faculty faculty = new Faculty(name, email, buildingName, officeNum, phoneNum, department, position);
+                FacultyList.add(faculty);
+            }
+        } finally {
+            
+            if (App.jsqlStmt != null) {
+                App.jsqlStmt.close();
+            }
+            if (App.jsqlConn != null) {
+            App.jsqlConn.close();
+            }
+        }
+    }
+             
+             
+              public static void updateSemesterList() throws SQLException {
+                String query = "SELECT * FROM Semesters"; 
+                App.runDBQuery(query, 'r'); 
+                
+                SemesterList.clear();  
+
+                try {
+                    while (App.jsqlResults.next()) {
+                    String period =  App.jsqlResults.getString(2);
+                    int year =  App.jsqlResults.getInt(3); 
+                  
+                    
+          
+            SemesterList.add(new Semester(period, year));
+        }
+    } finally {
+        
+        if (App.jsqlStmt != null) {
+            App.jsqlStmt.close();
+        }
+        if (App.jsqlConn != null) {
+            App.jsqlConn.close();
+        }
+    }
+} 
+              
+              
+              public static void updateEnrollmentList() throws SQLException {
+                String query = "SELECT * FROM ENROLLMENT"; 
+                App.runDBQuery(query, 'r');  
+                EnrollmentList.clear();  
+
+                try {
+                    while (App.jsqlResults.next()) {
+                    int courseID =  App.jsqlResults.getInt(1);
+                    int studentID =  App.jsqlResults.getInt(2); 
+                    int semesterID =  App.jsqlResults.getInt(3); 
+              
+                    Course course = null;
+                    for (Course curr: courseList){
+                        if (curr.courseID == courseID){
+                            course = curr;
+                            
+                        }
+                    }
+                    
+                    Student student = null;
+                    for (Student curr: StudentList){
+                        if (curr.studentID == studentID){  
+                            student = curr;
+                        }
+                    }
+                    
+                    Semester semester = null;
+                    for (Semester curr: SemesterList){
+                        if (curr.semesterID == semesterID){
+                            semester = curr;
+                        }
+                    }
+                    
+            // Add the course object to the list
+            EnrollmentList.add(new Enrollment(semester, course , student));
+        }
+    } finally {
+        
+        if (App.jsqlStmt != null) {
+            App.jsqlStmt.close();
+        }
+        if (App.jsqlConn != null) {
+            App.jsqlConn.close();
+        }
+    }
+}
+              
+                public static void updateScheduleList() throws SQLException {
+                String query = "SELECT * FROM SCHEDULE"; 
+                App.runDBQuery(query, 'r');  
+                ScheduleList.clear();  
+
+                try {
+                    while (App.jsqlResults.next()) {
+                    int courseID =  App.jsqlResults.getInt(2);
+                    int facultyID =  App.jsqlResults.getInt(3); 
+                    int semesterID =  App.jsqlResults.getInt(1); 
+              
+                    Course course = null;
+                    for (Course curr: courseList){
+                        if (curr.courseID == courseID){
+                            course = curr;
+                            
+                        }
+                    }
+                    
+                    Faculty faculty = null;
+                    for (Faculty curr: FacultyList){
+                        if (curr.facultyID == facultyID){  
+                            faculty = curr;
+                        }
+                    }
+                    
+                    Semester semester = null;
+                    for (Semester curr: SemesterList){
+                        if (curr.semesterID == semesterID){
+                            semester = curr;
+                        }
+                    }
+                    
+          
+            ScheduleList.add(new Schedule(semester, course , faculty));
+        }
+    } finally {
+        
+        if (App.jsqlStmt != null) {
+            App.jsqlStmt.close();
+        }
+        if (App.jsqlConn != null) {
+            App.jsqlConn.close();
+        }
+    }
+}
+
+}
+    
+
+    
    
     
-}
+
     
